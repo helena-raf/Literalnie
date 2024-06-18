36bad093193e66a76b0e51a11cb0674023eaa63c
helena-raf <helena.rafalowicz@gmail.com>
2024-06-18 22:57:41 +0200

    jakies zmiany w setupach paneli

--- Literalnie/src/Frame.java
+++ Literalnie/src/Frame.java
@@ -1,8 +1,10 @@
 import java.awt.event.*;
 import java.awt.*;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Frame extends JFrame {
@@ -15,25 +17,49 @@ public class Frame extends JFrame {
         this.guessGrid = new GuessGrid();
         this.ui = ui;
 
+        setUpMainPanel();
+        this.add(mainPanel);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(300, 400));;
+        this.setPreferredSize(new Dimension(300, 400));
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 handleKeyPressed(e);
             }
         });
-        this.add(mainPanel);
-        setUpMainPanel();
-        
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
     }
 
     private void setUpMainPanel() {
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        mainPanel.add(guessGrid);
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 3.0; 
+        gbc.fill = GridBagConstraints.BOTH;
+
+        mainPanel.add(guessGrid, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 4.0; 
+        gbc.fill = GridBagConstraints.BOTH;
+
+        JLabel alfabet = new JLabel();
+        alfabet.setOpaque(true);
+        alfabet.setBackground(Color.GREEN);
+        mainPanel.add(alfabet, gbc);
+
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     }
 
     public void handleKeyPressed(KeyEvent e) {
